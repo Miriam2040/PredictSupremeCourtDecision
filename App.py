abf58f5eabdb855ce626ab11e88c3183f23c89cf
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2021-02-28 18:47:37 +0200

    Update App.py

--- App.py
+++ App.py
@@ -18,58 +18,6 @@ archive = zipfile.ZipFile('model.zip', 'r')
 classifier = pickle.load(archive.open('model.pkl','r'))
 archive.close()
 
-def cache_on_button_press(label, **cache_kwargs):
-    """Function decorator to memoize function executions.
-    Parameters
-    ----------
-    label : str
-        The label for the button to display prior to running the cached funnction.
-    cache_kwargs : Dict[Any, Any]
-        Additional parameters (such as show_spinner) to pass into the underlying @st.cache decorator.
-    Example
-    -------
-    This show how you could write a username/password tester:
-    >>> @cache_on_button_press('Authenticate')
-    ... def authenticate(username, password):
-    ...     return username == "buddha" and password == "s4msara"
-    ...
-    ... username = st.text_input('username')
-    ... password = st.text_input('password')
-    ...
-    ... if authenticate(username, password):
-    ...     st.success('Logged in.')
-    ... else:
-    ...     st.error('Incorrect username or password')
-    """
-    internal_cache_kwargs = dict(cache_kwargs)
-    internal_cache_kwargs['allow_output_mutation'] = True
-    internal_cache_kwargs['show_spinner'] = False
-    
-    def function_decorator(func):
-        @functools.wraps(func)
-        def wrapped_func(*args, **kwargs):
-            @st.cache(**internal_cache_kwargs)
-            def get_cache_entry(func, args, kwargs):
-                class ButtonCacheEntry:
-                    def __init__(self):
-                        self.evaluated = False
-                        self.return_value = None
-                    def evaluate(self):
-                        self.evaluated = True
-                        self.return_value = func(*args, **kwargs)
-                return ButtonCacheEntry()
-            cache_entry = get_cache_entry(func, args, kwargs)
-            if not cache_entry.evaluated:
-                if st.button(label):
-                    cache_entry.evaluate()
-                else:
-                    st.stop()
-            return cache_entry.return_value
-        return wrapped_func
-    return function_decorator
-
-
-@cache_on_button_press('Predict')
 def prediction(issue, case_origin, case_source, cert_reason,law_type,natural_court,admin_action):   
    
     prediction = classifier.predict( 
@@ -96,11 +44,12 @@ def run_prediction():
     # the prediction function defined above is called to make the prediction  
     # and store it in the variable result 
     
-    if prediction(issue, case_origin, case_source, cert_reason,law_type,natural_court,admin_action) :
-       # if result == 1:
-        #    result = 'conservative'
-        #else:
-      	 #   result = 'liberal'		
+    if st.click('Predict):
+       result = prediction(issue, case_origin, case_source, cert_reason,law_type,natural_court,admin_action) :
+        if result == 1:
+            result = 'conservative'
+        else:
+      	    result = 'liberal'		
         result = 'yes'		
         st.success('US supreme court direction will be {}'.format(result)) 
 
