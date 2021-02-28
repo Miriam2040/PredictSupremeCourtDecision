a89f9d8349e953571e3778c105612e53aeaa3419
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2021-02-28 19:20:39 +0200

    Update App.py

--- App.py
+++ App.py
@@ -71,7 +71,7 @@ def main():
     elif app_mode == "About":
         image = Image.open('Team.PNG')
         st.image(image, caption='הצוות',use_column_width=True)
-        st.write("במסגרת קורס בינה מלאכותית ומוסר")
+        st.markdown("<h1 style='text-align: right; color: red;'>במסגרת פרויקט הסיום של קורס בינה מלאכותית</h1>", unsafe_allow_html=True)
       
    
      
