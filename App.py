72155972cdfd7c31381aa99fd5c579736833313b
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2021-03-02 13:24:39 +0200

    Update App.py

--- App.py
+++ App.py
@@ -82,7 +82,7 @@ def main():
         st.markdown("<h6 style='text-align: right;'> .לצורך כך לקחנו בעיה של חיזוי כיוון בית המשפט העליון בארה'ב ובנינו מודל בינה מלאכותית שיעשה זאת</h6>", unsafe_allow_html=True)
         st.markdown("<h6 style='text-align: right;'> .האתר הזה מסביר את העבודה שנעשתה ומנגיש את המודל לשימוש</h6>", unsafe_allow_html=True)
     elif app_mode == "Show Instructions":
-	st.header("The application includes following parts:")
+        st.header("The application includes following parts:")
 
 if __name__=='__main__': 
     main() 
