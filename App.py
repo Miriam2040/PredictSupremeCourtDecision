bce1e5d81284b2ea5414b173429382e0c88cbcd9
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2021-03-07 19:16:45 +0200

    Update App.py

--- App.py
+++ App.py
@@ -94,7 +94,7 @@ def main():
         col1.button('הטיה')
         col2.button('פרטיות')
         col3.button('לשון הרע')
-	st.markdown("<button style='color: blue;'></button>", unsafe_allow_html=True)
+        st.markdown("<button style='color: blue;'></button>", unsafe_allow_html=True)
         col11, col22, col33= st.beta_columns(3)
         col33.button('אך תחילה עלינו להבין את מקומה של הבינה המלאכותית בארצות הברית')
     elif app_mode == "Model Source Code":
